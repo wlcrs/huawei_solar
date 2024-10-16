d1204ca376949d5f702e4d25d8ccc536f219d007
Thomas H <harm112@freenet.de>
2024-10-16 20:54:18 +0200

    Added new battery entities, changed translations

--- number.py
+++ number.py
@@ -87,6 +87,15 @@ INVERTER_NUMBER_DESCRIPTIONS: tuple[HuaweiSolarNumberEntityDescription, ...] = (
         native_unit_of_measurement=UnitOfPower.WATT,
         entity_category=EntityCategory.CONFIG,
     ),
+    HuaweiSolarNumberEntityDescription(
+        key=rn.MPPT_SCANNING_INTERVAL,
+        native_max_value=30,
+        native_step=1,
+        native_min_value=5,
+        icon="mdi:sun-clock",
+        native_unit_of_measurement="minutes",
+        entity_category=EntityCategory.CONFIG,
+    ),
 )
 
 EMMA_NUMBER_DESCRIPTIONS: tuple[HuaweiSolarNumberEntityDescription, ...] = (
