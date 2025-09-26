d63b91b19b705366f8eeeb42bebe5cd1898188b9
perk737 <68247816+perk737@users.noreply.github.com>
2025-09-26 09:03:17 +0200

    Update services.py
    The existing version in develop does not work, at least not for me. This version works, that is I can set TOU time slots in my plant with EMMA installed. There might be better solutions, something for the repository owner to think about.

--- services.py
+++ services.py
@@ -8,6 +8,7 @@ import re
 from typing import TYPE_CHECKING, Any
 
 import voluptuous as vol
+from datetime import datetime
 
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant, ServiceCall, async_get_hass, callback
@@ -74,10 +75,21 @@ DATA_DURATION = "duration"
 DATA_TARGET_SOC = "target_soc"
 DATA_PERIODS = "periods"
 
+def validate_emma_device_id(device_id: str) -> str:
+    """Validate whether the device_id refers to a 'Connected Emma' device."""
+    hass = async_get_hass()
 
+    try:
+        _get_emma_bridge(hass, device_id)
+    except HuaweiSolarServiceException as err:
+        raise vol.Invalid(str(err)) from err
+    else:
+        return device_id
+    
 def validate_battery_device_id(device_id: str) -> str:
     """Validate whether the device_id refers to a 'Connected Energy Storage' device."""
     hass = async_get_hass()
+
     try:
         _get_battery_bridge(hass, device_id)
     except HuaweiSolarServiceException as err:
@@ -96,6 +108,16 @@ def validate_inverter_device_id(device_id: str) -> str:
     else:
         return device_id
 
+def validate_luna200_tou_string(tou_string) ->bool:
+    if not re.fullmatch(HUAWEI_LUNA2000_TOU_PATTERN, tou_string):
+        return False
+    times = re.findall(r'\d\d:\d\d', tou_string)
+    for time_str in times:
+        try:
+            datetime.strptime(time_str, "%H:%M")
+        except ValueError:
+            return False    
+    return True
 
 INVERTER_DEVICE_SCHEMA = vol.Schema(
     {DATA_DEVICE_ID: vol.All(cv.string, validate_inverter_device_id)}
@@ -104,7 +126,9 @@ INVERTER_DEVICE_SCHEMA = vol.Schema(
 BATTERY_DEVICE_SCHEMA = vol.Schema(
     {DATA_DEVICE_ID: vol.All(cv.string, validate_battery_device_id)}
 )
-
+EMMA_DEVICE_SCHEMA = vol.Schema(
+    {DATA_DEVICE_ID: vol.All(cv.string, validate_emma_device_id)}
+)
 
 FORCIBLE_CHARGE_BASE_SCHEMA = BATTERY_DEVICE_SCHEMA.extend(
     {
@@ -141,7 +165,7 @@ MAXIMUM_FEED_GRID_POWER_PERCENTAGE_SCHEMA = INVERTER_DEVICE_SCHEMA.extend(
 HUAWEI_LUNA2000_TOU_PATTERN = r"([0-2]\d:\d\d-[0-2]\d:\d\d/[1-7]{0,7}/[+-]\n?){0,14}"
 LG_RESU_TOU_PATTERN = r"([0-2]\d:\d\d-[0-2]\d:\d\d/\d+\.?\d*\n?){0,14}"
 
-TOU_PERIODS_SCHEMA = BATTERY_DEVICE_SCHEMA.extend(
+BATTERY_TOU_PERIODS_SCHEMA = BATTERY_DEVICE_SCHEMA.extend(
     {
         vol.Required(DATA_PERIODS): vol.All(
             cv.string,
@@ -149,7 +173,14 @@ TOU_PERIODS_SCHEMA = BATTERY_DEVICE_SCHEMA.extend(
         )
     }
 )
-
+EMMA_TOU_PERIODS_SCHEMA = EMMA_DEVICE_SCHEMA.extend(
+    {
+        vol.Required(DATA_PERIODS): vol.All(
+            cv.string,
+            vol.Match(HUAWEI_LUNA2000_TOU_PATTERN + r"|" + LG_RESU_TOU_PATTERN),
+        )
+    }
+)
 CAPACITY_CONTROL_PERIODS_PATTERN = (
     r"([0-2]\d:\d\d-[0-2]\d:\d\d/[1-7]{1,7}/\d+W\n?){0,14}"
 )
@@ -229,6 +260,36 @@ def _get_battery_bridge(
     _LOGGER.error("The provided device is not a Connected Energy Storage")
     raise HuaweiSolarServiceException("Not a valid 'Connected Energy Storage' device")
 
+@callback
+def _get_emma_bridge(
+    hass: HomeAssistant, device_id: str
+) -> tuple[HuaweiSUN2000Bridge, HuaweiSolarUpdateCoordinator]:
+    dev_reg = dr.async_get(hass)
+    device_entry = dev_reg.async_get(device_id)
+
+    if not device_entry:
+        raise HuaweiSolarServiceException("No such device found")
+
+    for entry_data in hass.data[DOMAIN].values():
+        hsucs: list[HuaweiSolarUpdateCoordinators] = entry_data[
+            DATA_UPDATE_COORDINATORS
+        ]
+        for uc in hsucs:
+            if uc.device_infos["emma"] is None:
+                continue
+            assert isinstance(uc.bridge, HuaweiEMMABridge)
+
+            device_info = uc.device_infos["emma"]
+            assert "identifiers" in device_info
+            for identifier in device_info["identifiers"]:
+                for device_identifier in device_entry.identifiers:
+                    if identifier == device_identifier:
+                        assert uc.configuration_update_coordinator
+                        return uc.bridge, uc.configuration_update_coordinator
+    _LOGGER.error("The provided device is not EMMA")
+    raise HuaweiSolarServiceException(
+        "Not a valid 'EMMA' device"
+    )
 
 @callback
 def get_battery_bridge(
@@ -298,6 +359,23 @@ def get_battery_or_emma_bridge(
 
     _LOGGER.info(
         "Got the following bridge and update_coordinators in get_battery_or_emma_bridge: %r, %r",
+        bridge,
+        uc,
+    )
+
+    return bridge, uc
+
+
+@callback
+def get_emma_bridge(
+    hass: HomeAssistant, service_call: ServiceCall
+) -> tuple[HuaweiEMMABridge, HuaweiSolarUpdateCoordinator]:
+    """Return the HuaweiSolarBridge associated with the EMMA device_id in the service call."""
+    device_id = service_call.data[DATA_DEVICE_ID]
+    bridge, uc = _get_emma_bridge(hass, device_id)
+
+    _LOGGER.info(
+        "Got the following bridge and update_coordinators in get_emma_bridge: %r, %r",
         bridge,
         uc,
     )
@@ -566,77 +644,81 @@ async def set_tou_periods(hass: HomeAssistant, service_call: ServiceCall) -> Non
     """Set the TOU periods of the battery."""
 
     def _parse_huawei_luna2000_periods(
-        text,
-    ) -> list[HUAWEI_LUNA2000_TimeOfUsePeriod]:
-        result = []
-        for line in text.split("\n"):
-            start_end_time_str, days_effective_str, charge_flag_str = line.split("/")
-            start_time_str, end_time_str = start_end_time_str.split("-")
+            text,
+        ) -> list[HUAWEI_LUNA2000_TimeOfUsePeriod]:
+            result = []
+            for line in text.split("\n"):
+                start_end_time_str, days_effective_str, charge_flag_str = line.split("/")
+                start_time_str, end_time_str = start_end_time_str.split("-")
 
-            result.append(
-                HUAWEI_LUNA2000_TimeOfUsePeriod(
-                    _parse_time(start_time_str),
-                    _parse_time(end_time_str),
-                    ChargeFlag.CHARGE
-                    if charge_flag_str == "+"
-                    else ChargeFlag.DISCHARGE,
-                    _parse_days_effective(days_effective_str),
+                result.append(
+                    HUAWEI_LUNA2000_TimeOfUsePeriod(
+                        _parse_time(start_time_str),
+                        _parse_time(end_time_str),
+                        ChargeFlag.CHARGE
+                        if charge_flag_str == "+"
+                        else ChargeFlag.DISCHARGE,
+                        _parse_days_effective(days_effective_str),
+                    )
                 )
-            )
 
-        return result
+            return result
 
     def _parse_lg_resu_periods(text) -> list[LG_RESU_TimeOfUsePeriod]:
-        result = []
-        for line in text.split("\n"):
-            start_end_time_str, energy_price = line.split("/")
-            start_time_str, end_time_str = start_end_time_str.split("-")
+            result = []
+            for line in text.split("\n"):
+                start_end_time_str, energy_price = line.split("/")
+                start_time_str, end_time_str = start_end_time_str.split("-")
 
-            result.append(
-                LG_RESU_TimeOfUsePeriod(
-                    _parse_time(start_time_str),
-                    _parse_time(end_time_str),
-                    float(energy_price),
+                result.append(
+                    LG_RESU_TimeOfUsePeriod(
+                        _parse_time(start_time_str),
+                        _parse_time(end_time_str),
+                        float(energy_price),
+                    )
                 )
+
+            return result
+
+    try:
+        bridge, uc = get_battery_or_emma_bridge(hass, service_call)
+        if isinstance(bridge, HuaweiEMMABridge):
+#          if not re.fullmatch(
+#               HUAWEI_LUNA2000_TOU_PATTERN , service_call.data[DATA_PERIODS]
+#           ):
+            if not validate_luna200_tou_string(service_call.data[DATA_PERIODS]):
+                raise ValueError(
+                    f"Invalid periods: validation failed for '{service_call.data[DATA_PERIODS]}' as TOU periods"
+                )
+            await bridge.set(
+                rn.EMMA_TOU_PERIODS,
+                _parse_huawei_luna2000_periods(service_call.data[DATA_PERIODS]),
+            )
+        elif bridge.battery_type == rv.StorageProductModel.HUAWEI_LUNA2000:
+#           if not re.fullmatch(
+#               HUAWEI_LUNA2000_TOU_PATTERN, service_call.data[DATA_PERIODS]
+#           ):
+            if not validate_luna200_tou_string(service_call.data[DATA_PERIODS]):
+                raise ValueError(
+                    f"Invalid periods: validation failed for '{service_call.data[DATA_PERIODS]}' as LUNA2000 TOU periods"
+                )
+            await bridge.set(
+                rn.STORAGE_HUAWEI_LUNA2000_TIME_OF_USE_CHARGING_AND_DISCHARGING_PERIODS,
+                _parse_huawei_luna2000_periods(service_call.data[DATA_PERIODS]),
+            )
+        elif bridge.battery_type == rv.StorageProductModel.LG_RESU:
+            if not re.fullmatch(LG_RESU_TOU_PATTERN, service_call.data[DATA_PERIODS]):
+                raise ValueError(
+                    f"Invalid periods: validation failed for '{service_call.data[DATA_PERIODS]}' as LG RESU TOU periods"
+                )
+            await bridge.set(
+                rn.STORAGE_LG_RESU_TIME_OF_USE_CHARGING_AND_DISCHARGING_PERIODS,
+                _parse_lg_resu_periods(service_call.data[DATA_PERIODS]),
             )
 
-        return result
-
-    bridge, uc = get_battery_or_emma_bridge(hass, service_call)
-
-    if isinstance(bridge, HuaweiEMMABridge):
-        if not re.fullmatch(
-            HUAWEI_LUNA2000_TOU_PATTERN, service_call.data[DATA_PERIODS]
-        ):
-            raise ValueError(
-                f"Invalid periods: validation failed for '{service_call.data[DATA_PERIODS]}' as TOU periods"
-            )
-        await bridge.set(
-            rn.EMMA_TOU_PERIODS,
-            _parse_huawei_luna2000_periods(service_call.data[DATA_PERIODS]),
-        )
-    elif bridge.battery_type == rv.StorageProductModel.HUAWEI_LUNA2000:
-        if not re.fullmatch(
-            HUAWEI_LUNA2000_TOU_PATTERN, service_call.data[DATA_PERIODS]
-        ):
-            raise ValueError(
-                f"Invalid periods: validation failed for '{service_call.data[DATA_PERIODS]}' as LUNA2000 TOU periods"
-            )
-        await bridge.set(
-            rn.STORAGE_HUAWEI_LUNA2000_TIME_OF_USE_CHARGING_AND_DISCHARGING_PERIODS,
-            _parse_huawei_luna2000_periods(service_call.data[DATA_PERIODS]),
-        )
-    elif bridge.battery_type == rv.StorageProductModel.LG_RESU:
-        if not re.fullmatch(LG_RESU_TOU_PATTERN, service_call.data[DATA_PERIODS]):
-            raise ValueError(
-                f"Invalid periods: validation failed for '{service_call.data[DATA_PERIODS]}' as LG RESU TOU periods"
-            )
-        await bridge.set(
-            rn.STORAGE_LG_RESU_TIME_OF_USE_CHARGING_AND_DISCHARGING_PERIODS,
-            _parse_lg_resu_periods(service_call.data[DATA_PERIODS]),
-        )
-
-    await uc.async_refresh()
+        await uc.async_refresh()
+    except ValueError as err:
+        raise vol.Invalid(str(err)) from err
 
 
 async def set_capacity_control_periods(
@@ -805,7 +887,7 @@ async def async_setup_services(
             DOMAIN,
             SERVICE_SET_TOU_PERIODS,
             partial(set_tou_periods, hass),
-            schema=TOU_PERIODS_SCHEMA,
+            schema=BATTERY_TOU_PERIODS_SCHEMA,
         )
 
         hass.services.async_register(
@@ -832,7 +914,7 @@ async def async_setup_services(
             DOMAIN,
             SERVICE_SET_TOU_PERIODS,
             partial(set_tou_periods, hass),
-            schema=TOU_PERIODS_SCHEMA,
+            schema=EMMA_TOU_PERIODS_SCHEMA,
         )
 
 
